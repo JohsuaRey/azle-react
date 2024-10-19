e7a205e5051b0602897abe35d13173e250b8bfcc
programmingdev19 dev <programmingdev19@gmail.com>
2024-10-19 12:18:48 +0000

    updated FE files

--- src/frontend/src/section/Navbar.tsx
+++ src/frontend/src/section/Navbar.tsx
@@ -22,21 +22,21 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav className={`container ${sticky ? 'bg-green-600 p-4 shadow-md fixed w-full z-50' : ''} `}>
+    <nav className={`container ${sticky ? 'bg-green-600 p-4 shadow-md fixed w-full z-50 ease-in duration-300' : ''} `}>
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-bold">GreenFuture</div>
         <div className="hidden md:flex space-x-6">
-          <a href="#home" className="text-white hover:text-gray-300 transition duration-300">
-            Home
+          <a href="#events" className="text-white hover:text-gray-300 transition duration-300">
+            Up comming event
           </a>
           <a href="#about" className="text-white hover:text-gray-300 transition duration-300">
-            About
+            Recent Event
           </a>
           <a href="#services" className="text-white hover:text-gray-300 transition duration-300">
-            Services
+            Climate Action
           </a>
           <a href="#contact" className="text-white hover:text-gray-300 transition duration-300">
-            Contact
+            Contact Us
           </a>
         </div>
         <div className="md:hidden">
