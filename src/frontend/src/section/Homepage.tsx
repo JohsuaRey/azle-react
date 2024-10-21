88b09781e8983e16193193609cae656bd830144b
programmingdev19 dev <programmingdev19@gmail.com>
2024-10-21 05:35:18 +0000

    updated FE files

--- src/frontend/src/section/Homepage.tsx
+++ src/frontend/src/section/Homepage.tsx
@@ -24,7 +24,7 @@ const Homepage = () => {
                     Join us at Greenfuture in the fight against climate change.
                 </p>
                 <div className='flex justify-center items-center mt-8'>
-                <a className="text-white hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-white dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Volunteer now</a>
+                <a href='#events' className="text-white hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-white dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Volunteer now</a>
                 </div>
             </div>
         </section>
